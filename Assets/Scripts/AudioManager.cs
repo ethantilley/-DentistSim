27edf34d70eafb75615d08c0a1ba652a7fe5a395
Ethan Tilley <1008464@student.sae.edu.au>
2019-05-17 16:20:28 +1000

    Implemented audiomanager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(this);
+        else
+            instance = this;
+    }
+
+
+    [System.Serializable]
+    public class SFXData
+    {
+        public string sfxName;
+        public AudioClip sfxClip;
+        [Range(0,3)]
+        public float volume = 0.1f;
+        public float pitchRand = 0;
+        public AudioSource deticatedSource;
+    }
+
+    public AudioSource sfxSource;
+
+    public SFXData[] SFX;
+
+    public void PlaySFX(string _name)
+    {
+        SFXData clip = SFX.Where(sfx => sfx.sfxName == _name).First();
+        if(clip.deticatedSource)
+        {
+            sfxSource.volume = clip.volume;
+            sfxSource.pitch = Random.Range(-clip.pitchRand, clip.pitchRand);
+            sfxSource.PlayOneShot(clip.sfxClip);
+        }
+        else
+        {
+            sfxSource.volume = clip.volume;
+            sfxSource.pitch = Random.Range(-clip.pitchRand, clip.pitchRand);
+            sfxSource.PlayOneShot(clip.sfxClip);
+                
+        }
+    }
+}
